9cce6616d4e70a19d5f6759c24b755cb98d5ae8f
Abhii <abhisheknalavade553@gmail.com>
2026-07-08 19:31:49 +0530

    Update professor.js

--- controllers/professor.js
+++ controllers/professor.js
@@ -6,37 +6,13 @@ require('exceljs');
 const Attendance =
 require('../models/attendance');
 const crypto = require("crypto");
-const nodemailer = require('nodemailer');
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
 
-const transporter = nodemailer.createTransport({
 
-    host: "smtp-relay.brevo.com",
+const { Resend } = require("resend");
 
-    port: 2525,
-
-    secure: false,
-
-    family: 4,
-    
-    auth: {
-
-        user: "b14bcc001@smtp-brevo.com",
-
-        pass: "xsmtpsib-31f7521a9fe1f881cbcf4aaa4cd4694df25ab9a7f5062301b182a6022c90f08b-mhAPIseHPAaIMdUg"
-
-    },
-
-        connectionTimeout: 15000,
-    greetingTimeout: 15000,
-    socketTimeout: 20000,
-
-    tls: {
-        rejectUnauthorized: false
-    }
-
-});
+const resend = new Resend(
+    process.env.RESEND_API_KEY
+);
 
 async function registerProfessor(req,res){
     const {fullname,email,department,password,invitecode} = req.body;
@@ -218,30 +194,30 @@ async function dispatchReport(req, res) {
 console.log("Professor:", professorEmail);
 console.log("Session:", sessionId);
 
-        await transporter.sendMail({
-            
-            from: '"AttendEase" <attendease.project123@gmail.com>',
+      
+   await resend.emails.send({
 
-            to:
-            professorEmail,
+    from: "onboarding@resend.dev",
 
-            subject:
-            "Attendance Report",
+    to: professorEmail,
 
-            text:
-            "Attendance report attached.",
+    subject: "Attendance Report",
 
-            attachments: [
+    text: "Attendance report attached.",
 
-                {
+    attachments: [
 
-                    filename: fileName,
-        content: buffer
-                }
+        {
 
-            ]
+            filename: fileName,
 
-        });
+            content: buffer.toString("base64")
+
+        }
+
+    ]
+
+});
 
         
 
